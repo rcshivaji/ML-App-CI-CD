b55c88e32560571069a75550c1bc654599f67b37
Shivaji <shivaji.raychaudhuri@postgrad.manchester.ac.uk>
2025-12-31 04:26:25 +0530

    Testing Pattern Change

--- tests/test_training.py
+++ tests/test_training.py
@@ -2,5 +2,4 @@ import os
 
 
 def test_model_artifacts_exist():
-    assert os.path.exists("models/active/model_v1.pkl")
-    assert os.path.exists("models/active/scaler_v1.pkl")
+    assert os.path.exists("models/active/model.pkl")
